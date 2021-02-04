3511a2ba1d9381f60342ce16e24d3109ad7aa4de
Martin C <martin.czabadaj@gmail.com>
2021-02-04 15:02:05 +0100

    project update

--- src/components/About.vue
+++ src/components/About.vue
@@ -31,14 +31,14 @@ export default {
     "paragraphs": {
       "first": "Anciennement professeur de mathématiques j'ai toujours eu un goût prononcé pour la programmation et la résolution de problèmes. J'ai donc décidé de quitter l'éducation pour poursuivre une carrière à plein temps dans le développement web.",
       "second": "Je suis developpeur full-stack autodidacte et j'aime créer des applications et sites web de toutes sortes. J'apprécie particulièrement les technologies VueJS et Express.",
-      "third": "N'hésitez pas à me contacter si vous avez un projet à me proposer ou que mes projets (j'en ajoute régulièrement) vous intéressent. Je suis principalement disponible pour des missions en freelance, mais je peux considérer un poste à plein temps en télétravail."
+      "third": "N'hésitez pas à me contacter si vous avez un projet à me proposer ou que mes projets (j'en ajoute régulièrement) vous intéressent."
     }
   },
   "en": {
     "paragraphs": {
       "first": "I'm a former maths teacher who always had a taste for solving problems and computer programming. This is why I decided to quit education to pursue a full time career in web developpement.",
       "second": "I'm a self-taught full stack developper and I like to create websites and apps of all kinds. I like using VueJS and Express.",
-      "third": "Don't hesitate to contact me if you have a project you would like me to work on, or if my projects (adding more regularly) interests you. I'm available on a freelance basis, but I would consider a full time remote job."
+      "third": "Don't hesitate to contact me if you have a project you would like me to work on, or if my projects (adding more regularly) interests you."
     }
   }
 }
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -12,13 +12,9 @@
           </p>
         </template>
         <template slot="intro">
-          <p>
-            {{ $t(`${project.title}.intro`) }}
-          </p>
+          <p>{{ $t(`${project.title}.intro`) }}</p>
         </template>
-        <p>
-          {{ $t(`${project.title}.description`) }}
-        </p>
+        <p>{{ $t(`${project.title}.description`) }}</p>
       </project-card>
     </div>
   </div>
@@ -61,6 +57,12 @@ export default {
           techs: "Nuxt, express, mySQL, Sequelize",
           imgSrc: "bulgneville.png",
           liveLink: "https://etude-cuny-morel.com"
+        },
+        {
+          title: "Hesperides",
+          techs: "Nuxt, express, mySQL, Sequelize, Vuetify",
+          imgSrc: "hesperides.png",
+          liveLink: "https://hesperides-cz.herokuapp.com/"
         }
       ]
     };
@@ -89,6 +91,10 @@ export default {
     "Bulgneville": {
       "intro": "Le site web de l'étude notaire de bulgnéville.",
       "description": "Site web professionnel avec un CMS permettant de poster des articles ou annonces immobilières. L'utilisateur peut consulter les articles, annonces immobilières et informations sur l'étude, envoyer des messages à l'étude directement via le site, et utiliser un calculateur de frais de notaire ou d'échéance de prêt immobilier"
+    },
+    "Hesperides": {
+      "intro": "Application web pour un syndic de copropriété",
+      "description": "Une application permettant de consulter et éditer une base de donnée sur les propriétaires, lots et locataires d'une copropriété. Permet également de poster des documents (factures/devis) au format pdf et de modifier leur statut (traité ou à traiter). \n\nSystème d'authentication complet avec 4 types de permissions (consultation de la DB, modification de la DB, poster des documents, consulter les documents). \n\nVous pouvez vous connecter avec les logs admin // admin et consulter la liste des comptes pour vous connecter avec d'autres permissions. (le mot de passe est toujours le même que le login) \n\nNB: Le système de documents ne fonctionne pas sur cette version vitrine à cause des limitations d'heroku."
     }
       
   },
@@ -108,6 +114,10 @@ export default {
     "Bulgneville": {
       "intro": "Bulgneville's notary office website.",
       "description": "Professionnal website with a CMS that let you post articles and real estate announces. The user can view articles, real estate announces, and the office appointments and informations about the office, send messages to the office directly from the website, and use a calculator for notary costs and real estate loan"
+    },
+    "Hesperides": {
+      "intro": "Web app made for a co-ownership buildings",
+      "description": "An app made for consulting and editing a database about owners, tenants, and appartement. Also features posting and consulting pdf document and editing their status to either 'to do' or 'done'. \n\nComplete authentication system with 4 types of authorizations (editing accounts, editing the database, posting documents and consulting documents). \n\nLogin using the credentials admin // admin to view the list of other accounts with different authorizations you can log on. (password is always equal to login)\n\nNB: Due to limitations on heroku hosting, the document features is not working on this showcase version."
     }
   }
 }
@@ -123,6 +133,9 @@ export default {
   justify-content: space-evenly;
   align-items: stretch;
   flex-wrap: wrap;
+  p {
+    white-space: pre-wrap;
+  }
 }
 
 @media screen and (max-width: 600px) {
